fd2be0bdad9e93918a6e0572e122dc0f347238f1
mohithaiyappa <mohithaiyappa229@gmail.com>
2020-10-31 14:36:58 +0530

    added retrofit api for delete and getDayEvents

--- app/src/main/java/com/example/myfitness/EventApi.java
+++ app/src/main/java/com/example/myfitness/EventApi.java
@@ -3,6 +3,7 @@ package com.example.myfitness;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -12,4 +13,14 @@ public interface EventApi {
     Call<List<Event>> getEvents(@Query("year") int year,
                                 @Query("month") int month,
                                 @Query("user_id") String userId);
+
+    @GET("devent.php")
+    Call<List<Event>> getDayEvents(@Query("year") int year,
+                                @Query("month") int month,
+                                @Query("user_id") String userId,
+                                @Query("day") String dayOfMonth);
+
+    @DELETE("del.php")
+    void deleteEvent(@Query("user_id") String userId,
+                     @Query("e_id") String e_id);
 }
