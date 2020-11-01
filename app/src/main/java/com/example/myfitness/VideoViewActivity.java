62a3f46d58a2bc4d6147c34321c431e89478983d
mohithaiyappa <mohithaiyappa229@gmail.com>
2020-11-01 12:37:24 +0530

    VideoViewActivity closes after videoEndTime

--- app/src/main/java/com/example/myfitness/VideoViewActivity.java
+++ app/src/main/java/com/example/myfitness/VideoViewActivity.java
@@ -1,10 +1,12 @@
 package com.example.myfitness;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.MediaController;
 import android.widget.Toast;
@@ -12,8 +14,14 @@ import android.widget.VideoView;
 
 import com.google.gson.Gson;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.TimeZone;
 
 public class VideoViewActivity extends AppCompatActivity {
     private Event event;
@@ -24,20 +32,25 @@ public class VideoViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_view);
+        ActionBar actionBar =getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
 
         String eventJsonString = getIntent().getStringExtra("event");
         if(!(eventJsonString == null || eventJsonString.isEmpty())) {
             event = new Gson().fromJson(eventJsonString, Event.class);
         }else {
-            Log.d("MyFitness229229", "event json data is empty ");
+            Log.d("MyFitness123456", "event json data is empty ");
         }
         if(event !=null){
             playVideo();
+            finishActivityAfterEventTime();
         }
     }
 
     private void playVideo() {
-        Log.d("MyFitness229229", "playing video "+event);
+        Log.d("MyFitness123456", "playing video "+event);
 
         List<String> urlArray = new ArrayList<>();
         String URL_1 = "https://ovp-vod.smartstream.ne.jp/hoankikaku/output/vod/ca5862191b9c4e12b16e36395c6248b8/78e3a5c478d84d24b27eabd6c4d96eb9/manifest.mp4";
@@ -70,5 +83,45 @@ public class VideoViewActivity extends AppCompatActivity {
         videoView.setVideoURI(uri);
         videoView.requestFocus();
         videoView.start();
+    }
+
+    private void finishActivityAfterEventTime(){
+        try {
+            Handler handler = new Handler();
+            String endTimeStr = getEndTime(event.getStartTime(), event.getVideoTime());
+            long endTime = getFinishTime(event.getEventDate(), endTimeStr).getTimeInMillis();
+            long runTime = endTime-Calendar.getInstance().getTimeInMillis();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    Log.d("MyFitness123456", "run: "+runTime);
+                    finish();
+                }
+            }, runTime);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
+    private Calendar getFinishTime(String stringDate, String stringTime) throws ParseException {
+        SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = dateFormat.parse(stringDate.trim()+" "+stringTime.trim());
+        if(date==null) {
+            throw new ParseException("error",3);
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
+    }
+
+    private String getEndTime(String startTime, String videoTime) throws ParseException {
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        timeFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        Date date1 = timeFormat.parse(startTime);
+        Date date2 = timeFormat.parse(videoTime);
+
+        long sum;
+        sum = date1.getTime() + date2.getTime();
+
+        return timeFormat.format(new Date(sum));
     }
 }
