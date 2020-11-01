4760c5b1439910558f5b9a2b95856ce4c6a3cdbc
mohithaiyappa <mohithaiyappa229@gmail.com>
2020-11-01 18:01:18 +0530

    VideoViewActivity.java url added to arraylist

--- app/src/main/java/com/example/myfitness/VideoViewActivity.java
+++ app/src/main/java/com/example/myfitness/VideoViewActivity.java
@@ -57,18 +57,7 @@ public class VideoViewActivity extends AppCompatActivity {
     }
 
     private void playVideo() {
-        Log.d("MyFitness123456", "playing video "+event);
-
-        String URL_1 = "https://ovp-vod.smartstream.ne.jp/hoankikaku/output/vod/ca5862191b9c4e12b16e36395c6248b8/78e3a5c478d84d24b27eabd6c4d96eb9/manifest.mp4";
-        String URL_2 ="https://ovp-vod.smartstream.ne.jp/hoankikaku/output/vod/c6d28fa266bf458a85250432637c2ae8/aaa28206f8f84d12b494df39a7aaced9/9xNzN3ZzE6QTqra_YBMSWoxOEDdhm4EE.mp4";
-        String URL_3 = "https://ovp-vod.smartstream.ne.jp/hoankikaku/output/vod/ca5862191b9c4e12b16e36395c6248b8/78e3a5c478d84d24b27eabd6c4d96eb9/manifest.mp4";
-        String URL_4 =  "https://ovp-vod.smartstream.ne.jp/hoankikaku/output/vod/6879b15b4e3a43e28518d75fe85820db/c8b306b9bf5647f9bb41298b5fdeeeca/manifest.mp4";
-        urlArray.add(URL_1);
-        urlArray.add(URL_2);
-        urlArray.add(URL_3);
-        urlArray.add(URL_4);
-
-
+        urlArray.add(event.getVideoUrl());
 
         videoView.setMediaController(new MediaController(this));
         videoView.requestFocus();
