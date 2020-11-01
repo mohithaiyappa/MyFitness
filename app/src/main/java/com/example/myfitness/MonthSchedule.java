490662f59b95ac8872285e77d794d8350b0b8f33
mohithaiyappa <mohithaiyappa229@gmail.com>
2020-11-01 21:39:42 +0530

    MonthSchedule.java cleanUp

--- app/src/main/java/com/example/myfitness/MonthSchedule.java
+++ app/src/main/java/com/example/myfitness/MonthSchedule.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,46 +14,17 @@ import android.widget.TextView;
 
 import android.widget.Button;
 import android.widget.GridView;
-import android.widget.Toast;
 
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MonthSchedule extends Fragment {
-
-    public final static String TAG = "MyFitness";
 
     private TextView titleText;
     private Button prevButton, nextButton;
     private CalendarAdapter mCalendarAdapter;
     private GridView calendarGridView;
-    /*private Callback callback = new Callback<List<Event>>(){
-
-        @Override
-        public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
-            if (!response.isSuccessful()){
-                Log.d(TAG, "onResponse: response not successful");
-                events = Collections.emptyList();
-                mCalendarAdapter.notifyDataSetChanged();
-                return;
-            }
-            events = response.body();
-            //Toast.makeText(getActivity(), events.size(), Toast.LENGTH_SHORT).show();
-            mCalendarAdapter.notifyDataSetChanged();
-        }
-
-        @Override
-        public void onFailure(Call<List<Event>> call, Throwable t) {
-            Log.d(TAG, "onFailure: "+t.toString());
-            events = Collections.emptyList();
-            mCalendarAdapter.notifyDataSetChanged();
-        }
-    };*/
 
     @Override
     public View onCreateView(
@@ -95,7 +65,6 @@ public class MonthSchedule extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Date selectedDate = (Date) mCalendarAdapter.getItem(position);
                 if(mCalendarAdapter.getDateManager().isCurrentMonth(selectedDate)) {
-                    //todo call repo method to set date
                     EventRepo.getInstance().loadDayEvents(selectedDate);
                 }
             }
