3901bf3553e362e9e6deeee5515ceceff2683f3b
mohithaiyappa <mohithaiyappa229@gmail.com>
2021-01-16 17:07:30 +0530

    StringUtils added

--- app/src/main/java/com/example/myfitness/utils/StringUtils.java
+++ app/src/main/java/com/example/myfitness/utils/StringUtils.java
@@ -3,6 +3,6 @@ package com.example.myfitness.utils;
 public class StringUtils {
 
     //event mode string literals
-    public static final String REPEAT_EVENT = "リピート";
-    public static final String ONE_DAY_EVENT = "本日のみ";
+    public static final String EVENT_MODE_REPEAT = "リピート";
+    public static final String EVENT_MODE_SINGLE = "本日のみ";
 }
