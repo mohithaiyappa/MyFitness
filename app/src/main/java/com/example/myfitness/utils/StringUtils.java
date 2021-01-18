714b4befe373f38b9295e9ebe5a414166549aa76
mohithaiyappa <mohithaiyappa229@gmail.com>
2021-01-18 14:32:06 +0530

    single event string changed

--- app/src/main/java/com/example/myfitness/utils/StringUtils.java
+++ app/src/main/java/com/example/myfitness/utils/StringUtils.java
@@ -4,5 +4,5 @@ public class StringUtils {
 
     //event mode string literals
     public static final String EVENT_MODE_REPEAT = "リピート";
-    public static final String EVENT_MODE_SINGLE = "本日のみ";
+    public static final String EVENT_MODE_SINGLE = "当日のみ";
 }
